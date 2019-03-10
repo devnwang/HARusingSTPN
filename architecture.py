f1526389a675ee7177d0a3140a159f49abc1536f
briantphan <briantphan25@gmail.com>
2019-03-10 15:28:16 -0700

    Add files via upload
    Update with Train model/STCB

--- architecture.py
+++ architecture.py
@@ -10,11 +10,15 @@ import torchvision.models as models
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.optim.lr_scheduler
+import Optical_Flow 
+import STCB
 
 seed = 42
 np.random.seed(seed)
 torch.manual_seed(seed)
 
+inputs = Optical_Flow.opt_flow()
 
 class STPN(torch.nn.Module):
 
@@ -30,7 +34,7 @@ class STPN(torch.nn.Module):
 		self.cnn4 = models.inception_v3(pretrained = True)
 
 		# Freeze the layers
-		for param in self.cnn1.parameters, self.cnn2.parameters(), self.cnn3.parameters, self.cnn4.parameters():
+		for param in self.cnn1.parameters(), self.cnn2.parameters(), self.cnn3.parameters(), self.cnn4.parameters():
 			param.requires_grad = False
 
 		# Construct last layer of CNNs
@@ -65,9 +69,16 @@ class STPN(torch.nn.Module):
 
 		# Attention stream
 		# STCB layers
-		# self.stcb1 = CompactBilinearPooling()
-		# self.stcb2 = CompactBilinearPooling()
+		self.stcb1 = STCB.CompactBilinearPooling(1024, 1024, 1024, 1024)
+		self.stcb1.cuda()
+		self.stcb1.train()
+		self.out1 = stcb1(cnn2,cnn3,cnn4)
 
+		self.stcb2 = STCB.CompactBilinearPooling(1024, 1024, 2048)
+		self.stcb2.cuda()
+		self.stcb2.train()
+		self.out2 = stcb2(out1,cnn1)
+		
 		# Input channels = 7, output channels = 7
 		self.conv1 = torch.nn.Conv2d(2048, 64, (7, 7), stride, padding)
 		self.conv2 = torch.nn.Conv2d(64, 1, (7, 7), stride, padding)
@@ -77,7 +88,11 @@ class STPN(torch.nn.Module):
 		self.pool3 = torch.nn.AvgPool2D((7, 7), stride, padding)
 
 		# Intersection of streams
-		# self.stcb3 = CompactBilinearPooling()
+		self.stcb3 = STCB.CompactBilinearPooling(1024,1024,1024,4096)
+		self.stcb3.cuda()
+		self.stcb3.train()
+		self.out3 = stcb3(pool1,pool2,pool3)
+		
 		# 4096 input features, 101 output features for 101 defined classes
 		self.fc = torch.nn.Linear(4096, 101)
 
@@ -105,13 +120,88 @@ class STPN(torch.nn.Module):
 
 		res = self.fc(spatemp)
 
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 		
 
 def createLossAndOptimizer(net, learning_rate = 0.001):
 	# Loss function
-	loss = torch.nn.CrossEntropyLoss()
+	criterion = torch.nn.CrossEntropyLoss()
 
 	# Optimizer
 	optimizer = optim.Adam(net.parameters(), lr = learning_rate)
 
-	return (loss, optimizer)
+	scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+	return (criterion, optimizer, scheduler)
+
+if __name__ == '__main__':
+    # arrFrames = Optical_Flow.opt_flow()
+    model_ft = train_model(STPN(), criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=25)
+    # test = STPN(arrFrames)
+    # print(test)
